3aaad3bc25bbff4c805480091c0911c111925e35
Francisco-93 <francisco.fro.agui@gmail.com>
2021-08-09 16:08:25 -0300

    validacao dos campos no formulario

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 
@@ -16,8 +16,8 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
+      email: ['', [Validators.email, Validators.required]],
+      fullName: ['', [Validators.required, Validators.minLength(5)]],
       userName: [''],
       password: ['']
     })
