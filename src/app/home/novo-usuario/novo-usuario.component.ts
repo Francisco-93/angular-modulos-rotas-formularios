85df7199ec3783334dfd327d1565c5bc4a829c3c
Francisco-93 <francisco.fro.agui@gmail.com>
2021-08-10 08:50:25 -0300

    adicionando novo usuario e redirecionando tela

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
@@ -13,7 +14,10 @@ export class NovoUsuarioComponent implements OnInit {
 
   novoUsuarioForm !: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
@@ -25,8 +29,15 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar(){
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario);
+    if(this.novoUsuarioForm.valid){
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastrarNovoUsuario(novoUsuario).subscribe(() => {
+        this.router.navigate(['']);
+      }, (error) => {
+        console.log(error);
+      })
+    }
+
   }
 
 }
---
+++ src/app/home/novo-usuario/usuario-existe.service.ts
@@ -0,0 +1,22 @@
+import { NovoUsuarioService } from './novo-usuario.service';
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { first, map, switchMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioExisteService {
+
+  constructor(private novoUsuarioService: NovoUsuarioService) { }
+
+  usuarioJaExiste(){
+    return( control: AbstractControl ) => {
+      control.valueChanges.pipe(
+        switchMap(nomeUsuario => this.novoUsuarioService.verificarUsuarioExistente(nomeUsuario)),
+        map(usuarioExiste => usuarioExiste ? {usuarioExistente: true} : null),
+        first()
+      );
+    }
+  }
+}
